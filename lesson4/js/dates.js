9f95115ed938fa83da0c278a9a6a124a8c6d1c45
mckennafaythe <mos16024@byui.edu>
2020-10-10 15:56:01 -0500

    update

--- lesson4/js/dates.js
+++ lesson4/js/dates.js
@@ -3,3 +3,19 @@ let d = new Date();
 document.getElementById("currentYear").textContent = d.getFullYear();
 
 document.getElementById("lastModifiedDate").textContent = document.lastModified;
+
+
+let today = new Date();
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentWeekday = days[today.getDay()];
+
+let date = today.getDate();
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let currentMonth = months[today.getMonth()];
+
+let currentYear = today.getFullYear();
+
+document.getElementById("todaysDate").innerHTML = currentWeekday + ", " + date + " " + currentMonth + " " + currentYear; 

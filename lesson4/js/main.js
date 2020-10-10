48aaca018063e8ccc07012c7afa5c25acc2bd642
mckennafaythe <mos16024@byui.edu>
2020-10-10 14:55:36 -0500

    update

--- lesson4/js/main.js
+++
@@ -1,9 +0,0 @@
-let d = new Date();
-
-document.getElementById("currentYear").textContent = d.getFullYear();
-
-document.getElementById("lastModifiedDate").textContent = document.lastModified;
-
-function toggleMenu() {
-    document.getElementById("mainNav").classList.toggle("hide");
-}

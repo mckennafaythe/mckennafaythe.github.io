a367a2cb0771f77b66d80697e6ee9796babde19f
mckennafaythe <mos16024@byui.edu>
2020-11-14 21:38:21 -0600

    update

---
+++ lesson9/js/cards.js
@@ -0,0 +1,48 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    
+    for (let i = 0; i < towns.length; i++) {
+        if (i == 1 || i == 4 || i == 5) {
+
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('p');
+        let yrFound = document.createElement('p');
+        let pop = document.createElement('p');
+        let rain = document.createElement('p');
+        let image = document.createElement('img');
+
+        h2.textContent = towns[i].name;
+        motto.textContent = towns[i].motto;
+        yrFound.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        pop.textContent = 'Population: ' + towns[i].currentPopulation;
+        rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        image.setAttribute('src', 'https://placeimg.com/640/480/nature');
+        image.setAttribute('alt', towns[i].name + ', Idaho');
+        motto.setAttribute('class', 'motto');
+        yrFound.setAttribute('class', 'townInfo');
+        pop.setAttribute('class', 'townInfo');
+        rain.setAttribute('class', 'townInfo');
+    
+        card.appendChild(h2);
+        card.appendChild(motto);
+        card.appendChild(yrFound);
+        card.appendChild(pop);
+        card.appendChild(rain);
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        }
+
+    }
+  });
+
+  
